d3a3edf4e082fad5cf956ca88b476c8dfa4c913f
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2020-09-29 15:25:15 +0200

    Updates

---
+++ precache-manifest.0dbe88cbad85ef75168fbf2fccce3c38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfad534f2638830b77b683383c027c10",
+    "url": "/js-lesson-4-chat/index.html"
+  },
+  {
+    "revision": "783bc369f24aba3ba8e9",
+    "url": "/js-lesson-4-chat/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "dd16997952c46f096138",
+    "url": "/js-lesson-4-chat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "783bc369f24aba3ba8e9",
+    "url": "/js-lesson-4-chat/static/js/2.e629aeb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-lesson-4-chat/static/js/2.e629aeb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd16997952c46f096138",
+    "url": "/js-lesson-4-chat/static/js/main.d8f0ca7f.chunk.js"
+  },
+  {
+    "revision": "61364ea6066847e49915",
+    "url": "/js-lesson-4-chat/static/js/runtime-main.2abc3676.js"
+  }
+]);
